5568dbae63b3aeef6a2a7f1015a18f02e48e270c
Magasoff <magasovramil@yandex.ru>
2023-01-07 21:42:54 +0300

    ДЗ2.

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,12 @@ public class Main {
     public static void main(String[] args) {
         Author author1  = new Author("Лев", "Толстой");
         Author author2  = new Author("Алексей", "Толстой");
+        Author author3  = new Author("Алексей", "Толстой");
         System.out.println(author1);
         System.out.println(author2);
         Book book = new Book ("Война и мир", author1, 2021);
         Book book2 = new Book ("Труд и война", author2, 2022);
+        Book book2 = new Book ("Труд и война", author2, 2024);
         System.out.println(book);
         System.out.println(book2);
     }
