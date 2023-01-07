8cd415e0907d0ce91f216749874bc39e797004e9
Magasoff <magasovramil@yandex.ru>
2023-01-07 21:40:41 +0300

    ДЗ2.

--- src/Author.java
+++ src/Author.java
@@ -9,7 +9,6 @@ public class Author {
         this.lastname = lastname;
     }
 
-
     public String getName() {
         return name;
     }
@@ -27,20 +26,25 @@ public class Author {
     }
 
     @Override
-    public String toString() {
-        return  name + " " + lastname;
-    }
-
-    @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Author author = (Author) o;
-        return name.equals(author.name) && lastname.equals(author.lastname);
+        return Objects.equals(name, author.name) && Objects.equals(lastname, author.lastname);
     }
 
     @Override
     public int hashCode() {
         return Objects.hash(name, lastname);
     }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "name='" + name + '\'' +
+                ", lastname='" + lastname + '\'' +
+                '}';
+    }
 }
+
+
--- src/Book.java
+++ src/Book.java
@@ -5,7 +5,6 @@ public class Book {
     private Author author;
     private int published;
 
-
     public Book(String title, Author author, int published) {
         this.title = title;
         this.author = author;
@@ -37,20 +36,24 @@ public class Book {
     }
 
     @Override
-    public String toString() {
-        return  " Название книги " + title + ", Автор " + author + ", Год издания " + published;
-    }
-
-    @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Book book = (Book) o;
-        return published == book.published && title.equals(book.title) && author.equals(book.author);
+        return published == book.published && Objects.equals(title, book.title) && Objects.equals(author, book.author);
     }
 
     @Override
     public int hashCode() {
         return Objects.hash(title, author, published);
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", author=" + author +
+                ", published=" + published +
+                '}';
     }
 }
